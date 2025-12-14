24b1fc3dc0fca2de83f49144108ee08d7239b89a
Jettan17 <jethrotan2006@gmail.com>
2025-12-14 14:00:29 +0800

    feat: added API call function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+async function getWeatherData(location) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup = metric&key=Q9VPWA64VU6LM9FM36Y3ZAXJW&contentType=json`);
+    
+    const jsonData = await response.json()
+
+    console.log(jsonData)
+}
+
+getWeatherData("Singapore");
+getWeatherData("Malaysia");
+getWeatherData("France");
