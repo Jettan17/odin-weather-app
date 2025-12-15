ec4814f1818cbc31e878ebdf159f1f537c3441d0
Jettan17 <jethrotan2006@gmail.com>
2025-12-15 14:59:04 +0800

    feat: processed jsonData

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import "./style.css";
 
 async function getWeatherData(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup = metric&key=Q9VPWA64VU6LM9FM36Y3ZAXJW&contentType=json`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=Q9VPWA64VU6LM9FM36Y3ZAXJW&contentType=json`);
     
     const jsonData = await response.json()
-
-    console.log(jsonData)
+    
+    return processWeatherData(jsonData);
 }
 
-getWeatherData("Singapore");
-getWeatherData("Malaysia");
-getWeatherData("France");
+function processWeatherData(jsonData) {
+    const finalWeatherData = { //Only process relevant attributes
+        address: jsonData.address,
+        description: jsonData.description,
+        currentConditions: {
+            conditions: jsonData.currentConditions.conditions,
+            datetime: new Date().toISOString().slice(0, 10),
+            feelslike: jsonData.currentConditions.feelslike,
+            humidity: jsonData.currentConditions.humidity,
+            icon: jsonData.currentConditions.icon,
+            temp: jsonData.currentConditions.temp
+        },
+        dayConditions: [...jsonData.days.map(day => ({
+            conditions: day.conditions,
+            datetime: day.datetime,
+            feelslike: day.feelslike,
+            humidity: day.humidity,
+            icon: day.icon,
+            temp: day.temp
+        }))]
+    }
+
+    return finalWeatherData;
+}
+
+console.log(getWeatherData("Singapore"));
+console.log(getWeatherData("Malaysia"));
+console.log(getWeatherData("France"));
