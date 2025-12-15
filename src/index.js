a8e093b4cd083bd57749ba71eeb19c03f58391e3
Jettan17 <jethrotan2006@gmail.com>
2025-12-15 15:23:16 +0800

    feat: added form logic

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import "./style.css";
 async function getWeatherData(location) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=Q9VPWA64VU6LM9FM36Y3ZAXJW&contentType=json`);
     
-    const jsonData = await response.json()
+    console.log(response.status);
+    if (response.status.toString()[0] === "2") {
+        const jsonData = await response.json();
     
-    return processWeatherData(jsonData);
-}
+        return processWeatherData(jsonData);
+    } else {
+        return null;
+    }
+};
 
 function processWeatherData(jsonData) {
     const finalWeatherData = { //Only process relevant attributes
@@ -28,11 +33,23 @@ function processWeatherData(jsonData) {
             icon: day.icon,
             temp: day.temp
         }))]
-    }
+    };
 
     return finalWeatherData;
-}
+};
 
-console.log(getWeatherData("Singapore"));
-console.log(getWeatherData("Malaysia"));
-console.log(getWeatherData("France"));
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("location-form");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+
+        const dataObject = Object.fromEntries(formData.entries());
+
+        console.log(dataObject["location-input"]);
+        const weatherData = getWeatherData(dataObject["location-input"]);
+        console.log(weatherData);
+    });
+});
